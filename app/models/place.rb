c95167793f361e777f7b1ba0e7d3fee835775b04
Melanie Locke <melanie.a.locke@gmail.com>
2019-09-03 18:27:44 +0000

    Added validations to address and description fields in new place form

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,5 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :address, :description, presence: true
 end
