2194a1c0c24b727a5c2bc79d7da197720ca7aef5
Melanie Locke <melanie.a.locke@gmail.com>
2019-09-10 23:18:32 +0000

    Added specific place info to comment email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,10 +1,12 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
-  def comment_added
-    mail(to: "letstestthismess@gmail.com",
-          subject: "A comment has been added to your place")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
 
+    mail(to: @place_owner.email,
+          subject: "A comment has been added to #{@place.name}")
   end
 
 end
